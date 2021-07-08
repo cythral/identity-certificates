2cf1225d1d05dc9410be3c76a75d0d97aec638d1
Talen Fisher <talen.fisher@cythral.com>
2021-07-08 07:09:57 -0500

    Switch to ECDSA (#7)

--- src/CertificateRotator/Services/ICertificateFactory.cs
+++ src/CertificateRotator/Services/ICertificateFactory.cs
@@ -11,17 +11,13 @@ namespace Brighid.Identity.Certificates.CertificateRotator
         /// Creates a new signing certificate.
         /// </summary>
         /// <param name="domain">The distinguished name to use for the certificate.</param>
-        /// <param name="keyLength">The key length to use for the certificate.</param>
         /// <param name="hashAlgorithm">The hash algorithm to use for the certificate.</param>
-        /// <param name="padding">Padding to use for the certificate.</param>
         /// <param name="days">Number of days for the certificate to be valid for.</param>
         /// <returns>The resulting certificate bytes.</returns>
         CertificateInfo CreateSigningCertificate(
             string domain,
-            int keyLength,
             HashAlgorithmName hashAlgorithm,
-            RSASignaturePadding padding,
-            uint days
+            int days
         );
     }
 }
