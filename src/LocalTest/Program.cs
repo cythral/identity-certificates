e8cc4d6011035d639f999fc070b77a854742a94a
Talen Fisher <talen.fisher@cythral.com>
2021-11-17 20:11:43 -0600

    .NET Update, Automatic Layer Updates & Contract Tests (#90)

--- src/LocalTest/Program.cs
+++ src/LocalTest/Program.cs
@@ -1,6 +1,9 @@
+using System.IO;
 using System.Threading.Tasks;
 
 using Brighid.Identity.Certificates.CertificateRotator;
+
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 #pragma warning disable CS1591
 #pragma warning disable SA1600
@@ -12,7 +15,10 @@ namespace Brighid.Identity.Certificates.LocalTest
     {
         public static async Task Main(string[] args)
         {
-            await Handler.Run(new CertificateRotator.RotateCertificateRequest(), null!);
+            var stream = new MemoryStream();
+            await JsonSerializer.SerializeAsync(stream, new RotateCertificateRequest());
+            stream.Position = 0;
+            await Handler.Run(stream, null!);
         }
     }
 }
